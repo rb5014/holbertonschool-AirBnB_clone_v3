b8162fa2cadcc082a07d520f988ce759f532f399
Romain Bayle <5014@holbertonstudents.com>
2022-12-08 10:58:52 +0100

    added methods in route

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,6 @@
 """Blueprint views"""
 from flask import Blueprint
 
-app_views = Blueprint(name="app_views",
-                      import_name=__name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
