9458b09afd67b1a1ae49d50f130c0468230b42bb
Romain Bayle <5014@holbertonstudents.com>
2022-12-08 13:31:07 +0100

    task 6 not found error handler in app.py + removed jsonify because automatically converted to json when dict is returned

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Index fiile of the api"""
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """get status"""
-    return jsonify(status='OK')
+    return {"status": "OK"}
 
 
 @app_views.route("/stats",
