f002de99325244e5d932f8a24a4ca606031887f7
Romain Bayle <5014@holbertonstudents.com>
2022-12-08 10:55:49 +0100

    fixed jsonify instead of json.dumps for task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Index fiile of the api"""
 from api.v1.views import app_views
-import json
+from flask import jsonify
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
-    return json.dumps({"status": "OK"}, indent=4)
+    """get status"""
+    return jsonify(status="OK")
