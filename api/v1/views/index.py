4fa8edd35d6d749b38cdba9e5685c53b2bc0ad4c
Romain Bayle <5014@holbertonstudents.com>
2022-12-08 13:17:31 +0100

    route stats added to index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """Index fiile of the api"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """get status"""
     return jsonify(status='OK')
+
+
+@app_views.route("/stats",
+                 methods=['GET'], strict_slashes=False)
+def count():
+    classes = {"amenities": "Amenity",
+               "cities": "City",
+               "places": "Place",
+               "reviews": "Review",
+               "states": "State",
+               "users": "User"}
+    for k, v in classes.items():
+        classes[k] = storage.count(v)
+    return classes
